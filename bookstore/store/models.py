38cea6edff6c8280a7c76adce38f3a96ae7b081c
ASWINDAS <cvaswin1997@gmail.com>
2023-10-12 18:36:37 +0530

    logo changed

--- bookstore/store/models.py
+++ bookstore/store/models.py
@@ -60,10 +60,9 @@ class Cart(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     product_qty = models.IntegerField(null=False,blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+        
     def __str__(self):
         return self.user.username
-    
 class Wishlist(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
